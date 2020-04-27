06ebf164f723c07088e1d0260cef2d4e18ff7b90
Raja Khan <rajashahidullah@hotmail.com>
2020-04-27 11:28:27 +0200

    Corrected

--- HoofdStuk_Lambda_Expression/src/Random_Excercise/SquareExcercise.java
+++ HoofdStuk_Lambda_Expression/src/Random_Excercise/SquareExcercise.java
@@ -5,30 +5,21 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
 import static java.util.stream.Collectors.toList;
-
 
 public class SquareExcercise {
 
 	public static void main(String[] args) {
-	 IntStream.range(0, 1001).filter(f->f%f==0).filter(f->f<21).filter(f->f%5==0).filter(f->f%8==0).forEach(System.out::println);	
-			
-		
-				
-			
-	
-
-		
-		
-		
-		
-		
-		
-			
-		}
-	    
+		double myStream = IntStream.rangeClosed(1, 1000)
+				.filter(f -> f % 8 == 0 && f % 5 == 0)
+				.limit(20)
+				.mapToDouble(Math::sqrt)
+				.sum();
+		 
+		System.out.println(myStream);
 
 	}
 
-
+}
